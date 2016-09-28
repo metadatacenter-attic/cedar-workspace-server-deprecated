6b9888fef64cef55454ee195f96906711a6e949a
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-09-28 13:22:14 -0700

    Removed unnecessary code

--- cedar-folder-server-core/src/main/java/org/metadatacenter/rest/CedarRequestContextFactory.java
+++
@@ -1,9 +0,0 @@
-package org.metadatacenter.rest;
-
-import play.mvc.Http;
-
-public class CedarRequestContextFactory {
-  public static ICedarRequestContext fromRequest(Http.Request request) {
-    return new PlayRequestContext(request);
-  }
-}
