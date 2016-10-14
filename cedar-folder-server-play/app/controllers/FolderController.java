14bf53fb204d11e704f1b3949a4bd069b79e47e3
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-10-14 11:28:46 -0700

    Permission handling moved back from permission server

--- cedar-folder-server-play/app/controllers/FolderController.java
+++ cedar-folder-server-play/app/controllers/FolderController.java
@@ -8,10 +8,13 @@ import org.metadatacenter.model.folderserver.CedarFSFolder;
 import org.metadatacenter.model.folderserver.CedarFSNode;
 import org.metadatacenter.server.neo4j.Neo4JUserSession;
 import org.metadatacenter.server.neo4j.NodeLabel;
+import org.metadatacenter.server.result.BackendCallResult;
 import org.metadatacenter.server.security.Authorization;
 import org.metadatacenter.server.security.CedarAuthFromRequestFactory;
 import org.metadatacenter.server.security.exception.CedarAccessException;
 import org.metadatacenter.server.security.model.IAuthRequest;
+import org.metadatacenter.server.security.model.auth.CedarNodePermissions;
+import org.metadatacenter.server.security.model.auth.CedarNodePermissionsRequest;
 import org.metadatacenter.server.security.model.auth.CedarPermission;
 import org.metadatacenter.server.security.model.auth.NodePermission;
 import org.metadatacenter.server.security.model.user.CedarUser;
@@ -296,6 +299,81 @@ public class FolderController extends AbstractFolderServerController {
       }
     } catch (Exception e) {
       play.Logger.error("Error while deleting the folder", e);
+      return internalServerErrorWithError(e);
+    }
+  }
+
+  public static Result getPermissions(String folderId) {
+    IAuthRequest frontendRequest = null;
+    CedarUser currentUser = null;
+    try {
+      frontendRequest = CedarAuthFromRequestFactory.fromRequest(request());
+      currentUser = Authorization.getUserAndEnsurePermission(frontendRequest, CedarPermission.LOGGED_IN);
+    } catch (CedarAccessException e) {
+      play.Logger.error("Access Error while reading the permissions of folder", e);
+      return forbiddenWithError(e);
+    }
+
+    try {
+      Neo4JUserSession neoSession = DataServices.getInstance().getNeo4JSession(currentUser);
+
+      CedarFSFolder folder = neoSession.findFolderById(folderId);
+      if (folder == null) {
+        ObjectNode errorParams = JsonNodeFactory.instance.objectNode();
+        errorParams.put("id", folderId);
+        return notFound(generateErrorDescription("folderNotFound",
+            "The folder can not be found by id:" + folderId, errorParams));
+      } else {
+        CedarNodePermissions permissions = neoSession.getNodePermissions(folderId, true);
+        JsonNode permissionsNode = JsonMapper.MAPPER.valueToTree(permissions);
+        return ok(permissionsNode);
+      }
+    } catch (Exception e) {
+      play.Logger.error("Error while getting the folder", e);
+      return internalServerErrorWithError(e);
+    }
+  }
+
+  public static Result updatePermissions(String folderId) {
+    IAuthRequest frontendRequest = null;
+    CedarUser currentUser = null;
+    try {
+      frontendRequest = CedarAuthFromRequestFactory.fromRequest(request());
+      currentUser = Authorization.getUserAndEnsurePermission(frontendRequest, CedarPermission.LOGGED_IN);
+    } catch (CedarAccessException e) {
+      play.Logger.error("Access Error while updating the folder permissions", e);
+      return forbiddenWithError(e);
+    }
+
+    try {
+      JsonNode permissionUpdateRequest = request().body().asJson();
+      if (permissionUpdateRequest == null) {
+        return badRequest(generateErrorDescription("missingRequestBody",
+            "You must supply the request body as a json object!"));
+      }
+
+      Neo4JUserSession neoSession = DataServices.getInstance().getNeo4JSession(currentUser);
+
+      CedarNodePermissionsRequest permissionsRequest = JsonMapper.MAPPER.treeToValue(permissionUpdateRequest,
+          CedarNodePermissionsRequest.class);
+
+      CedarFSFolder folder = neoSession.findFolderById(folderId);
+      if (folder == null) {
+        ObjectNode errorParams = JsonNodeFactory.instance.objectNode();
+        errorParams.put("id", folderId);
+        return notFound(generateErrorDescription("folderNotFound",
+            "The folder can not be found by id:" + folderId, errorParams));
+      } else {
+        BackendCallResult backendCallResult = neoSession.updateNodePermissions(folderId, permissionsRequest, true);
+        if (backendCallResult.isError()) {
+          return backendCallError(backendCallResult);
+        }
+        CedarNodePermissions permissions = neoSession.getNodePermissions(folderId, true);
+        JsonNode permissionsNode = JsonMapper.MAPPER.valueToTree(permissions);
+        return ok(permissionsNode);
+      }
+    } catch (Exception e) {
+      play.Logger.error("Error while updating the folder permissions", e);
       return internalServerErrorWithError(e);
     }
   }
