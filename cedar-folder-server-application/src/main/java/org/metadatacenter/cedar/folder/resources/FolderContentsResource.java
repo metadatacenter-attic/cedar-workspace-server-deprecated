c9e9f0fa9162ec57687709da86ba1fe37caa2b26
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-01-29 15:07:00 -0800

    Refactor paging param validation

--- cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/FolderContentsResource.java
+++ cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/FolderContentsResource.java
@@ -1,7 +1,6 @@
 package org.metadatacenter.cedar.folder.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import org.apache.commons.lang3.StringUtils;
 import org.metadatacenter.bridge.CedarDataServices;
 import org.metadatacenter.config.CedarConfig;
 import org.metadatacenter.error.CedarErrorKey;
@@ -16,18 +15,16 @@ import org.metadatacenter.model.response.FolderServerNodeListResponse;
 import org.metadatacenter.rest.context.CedarRequestContext;
 import org.metadatacenter.rest.context.CedarRequestContextFactory;
 import org.metadatacenter.server.FolderServiceSession;
-import org.metadatacenter.server.neo4j.FolderContentSortOptions;
 import org.metadatacenter.util.http.CedarResponse;
 import org.metadatacenter.util.http.CedarUrlUtil;
 import org.metadatacenter.util.http.LinkHeaderUtil;
+import org.metadatacenter.util.http.PagedSearchQuery;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -144,74 +141,13 @@ public class FolderContentsResource extends AbstractFolderServerResource {
                                       Optional<Integer> limitParam, Optional<Integer> offsetParam) throws
       CedarException {
 
-    // Test limit
-    // TODO : set defaults from config here
-    int limit = 50; // set default
-    if (limitParam.isPresent()) {
-      if (limitParam.get() <= 0) {
-        throw new IllegalArgumentException("You should specify a positive limit!");
-      } else if (limitParam.get() > 100) {
-        throw new IllegalArgumentException("You should specify a limit smaller than 100!");
-      }
-      limit = limitParam.get();
-    }
+    PagedSearchQuery psq = new PagedSearchQuery(resourceTypes, sort, limitParam, offsetParam);
+    psq.validate();
 
-    // Test offset
-    int offset = 0;
-    if (offsetParam.isPresent()) {
-      if (offsetParam.get() < 0) {
-        throw new IllegalArgumentException("You should specify a positive or zero offset!");
-      }
-      offset = offsetParam.get();
-    }
-
-    // Test sort
-    String sortString;
-    if (sort.isPresent()) {
-      sortString = sort.get();
-    } else {
-      sortString = FolderContentSortOptions.getDefaultSortField().getName();
-    }
-
-    if (sortString != null) {
-      sortString = sortString.trim();
-    }
-
-    List<String> sortList = Arrays.asList(StringUtils.split(sortString, ","));
-    for (String s : sortList) {
-      String test = s;
-      if (s != null && s.startsWith("-")) {
-        test = s.substring(1);
-      }
-      if (!FolderContentSortOptions.isKnownField(test)) {
-        throw new IllegalArgumentException("You passed an illegal sort type:'" + s + "'. The allowed values are:" +
-            FolderContentSortOptions.getKnownFieldNames());
-      }
-    }
-
-    // Test resourceTypes
-    String nodeTypesString = null;
-    if (resourceTypes.isPresent()) {
-      nodeTypesString = resourceTypes.get();
-    }
-    if (nodeTypesString != null) {
-      nodeTypesString = nodeTypesString.trim();
-    }
-    if (nodeTypesString == null || nodeTypesString.isEmpty()) {
-      throw new IllegalArgumentException("You must pass in resource_types as a comma separated list!");
-    }
-
-    List<String> nodeTypeStringList = Arrays.asList(StringUtils.split(nodeTypesString, ","));
-    List<CedarNodeType> nodeTypeList = new ArrayList<>();
-    for (String rt : nodeTypeStringList) {
-      CedarNodeType crt = CedarNodeType.forValue(rt);
-      if (crt == null) {
-        throw new IllegalArgumentException("You passed an illegal sort type:'" + rt + "'. The allowed values are:" +
-            CedarNodeType.values());
-      } else {
-        nodeTypeList.add(crt);
-      }
-    }
+    int limit = psq.getLimit();
+    int offset = psq.getOffset();
+    List<String> sortList = psq.getSortList();
+    List<CedarNodeType> nodeTypeList = psq.getNodeTypeList();
 
     FolderServerNodeListResponse r = new FolderServerNodeListResponse();
     r.setNodeListQueryType(NodeListQueryType.FOLDER_CONTENT);
--- cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/FoldersResource.java
+++ cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/FoldersResource.java
@@ -360,31 +360,4 @@ public class FoldersResource extends AbstractFolderServerResource {
       return Response.ok().entity(permissions).build();
     }
   }
-
-  /*
-  protected static Integer ensureLimit(Integer limit) {
-    return limit == null ? cedarConfig.getFolderRESTAPI().getPagination().getDefaultPageSize() : limit;
-  }
-
-  protected static void checkPagingParameters(Integer limit, Integer offset) {
-    // check offset
-    if (offset < 0) {
-      throw new IllegalArgumentException("Parameter 'offset' must be positive!");
-    }
-    // check limit
-    if (limit <= 0) {
-      throw new IllegalArgumentException("Parameter 'limit' must be greater than zero!");
-    }
-    int maxPageSize = cedarConfig.getFolderRESTAPI().getPagination().getDefaultPageSize();
-    if (limit > maxPageSize) {
-      throw new IllegalArgumentException("Parameter 'limit' must be at most " + maxPageSize + "!");
-    }
-  }
-
-  protected static void checkPagingParametersAgainstTotal(Integer offset, long total) {
-    if (offset != 0 && offset > total - 1) {
-      throw new IllegalArgumentException("Parameter 'offset' must be smaller than the total count of objects, which " +
-          "is " + total + "!");
-    }
-  }*/
 }
