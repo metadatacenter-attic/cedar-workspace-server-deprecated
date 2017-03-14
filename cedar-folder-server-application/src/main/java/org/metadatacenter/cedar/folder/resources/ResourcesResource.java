12dbe6429152788a440ae5082c05b1ea65f804a4
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-03-14 15:47:33 -0700

    Refactored Neo4j handling with types

--- cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/ResourcesResource.java
+++ cedar-folder-server-application/src/main/java/org/metadatacenter/cedar/folder/resources/ResourcesResource.java
@@ -18,6 +18,7 @@ import org.metadatacenter.rest.context.CedarRequestContextFactory;
 import org.metadatacenter.server.FolderServiceSession;
 import org.metadatacenter.server.PermissionServiceSession;
 import org.metadatacenter.server.neo4j.NodeLabel;
+import org.metadatacenter.server.neo4j.parameter.NodeProperty;
 import org.metadatacenter.server.result.BackendCallResult;
 import org.metadatacenter.server.security.model.auth.CedarNodePermissions;
 import org.metadatacenter.server.security.model.auth.CedarNodePermissionsRequest;
@@ -206,13 +207,13 @@ public class ResourcesResource extends AbstractFolderServerResource {
           .errorMessage("The resource can not be found by id")
           .build();
     } else {
-      Map<String, String> updateFields = new HashMap<>();
+      Map<NodeProperty, String> updateFields = new HashMap<>();
       if (description != null) {
-        updateFields.put("description", descriptionV);
+        updateFields.put(NodeProperty.DESCRIPTION, descriptionV);
       }
       if (name != null) {
-        updateFields.put("name", nameV);
-        updateFields.put("displayName", nameV);
+        updateFields.put(NodeProperty.NAME, nameV);
+        updateFields.put(NodeProperty.DISPLAY_NAME, nameV);
       }
       FolderServerResource updatedResource = folderSession.updateResourceById(id, resource.getType(), updateFields);
       if (updatedResource == null) {
