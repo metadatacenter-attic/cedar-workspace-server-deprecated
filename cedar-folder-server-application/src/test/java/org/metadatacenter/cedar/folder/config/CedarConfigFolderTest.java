bd9d538d452bc1e7bea58e291a1eacf769be96b9
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-08 14:55:42 -0700

    Refactor environment variables related to Docker

--- cedar-folder-server-application/src/test/java/org/metadatacenter/cedar/folder/config/CedarConfigFolderTest.java
+++ cedar-folder-server-application/src/test/java/org/metadatacenter/cedar/folder/config/CedarConfigFolderTest.java
@@ -22,23 +22,18 @@ public class CedarConfigFolderTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_ADMIN_USER_API_KEY.getName(), "1234");
 
-    env.put(CedarEnvironmentVariable.CEDAR_NEO4J_USER_NAME.getName(), "name");
     env.put(CedarEnvironmentVariable.CEDAR_NEO4J_USER_PASSWORD.getName(), "password");
     env.put(CedarEnvironmentVariable.CEDAR_NEO4J_HOST.getName(), "127.0.0.1");
     env.put(CedarEnvironmentVariable.CEDAR_NEO4J_REST_PORT.getName(), "7474");
-
-    env.put(CedarEnvironmentVariable.CEDAR_KEYCLOAK_CLIENT_ID.getName(), "cedar-angular-app");
 
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME.getName(), "cedarUser");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD.getName(), "password");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_PORT.getName(), "27017");
 
-    env.put(CedarEnvironmentVariable.CEDAR_PORT_FOLDER.getName(), "9008");
-
-    env.put(CedarEnvironmentVariable.CEDAR_LD_USER_BASE.getName(), "https://metadatacenter.org/users/");
-
-    env.put(CedarEnvironmentVariable.CEDAR_EVERYBODY_GROUP_NAME.getName(), "Everybody");
+    env.put(CedarEnvironmentVariable.CEDAR_FOLDER_HTTP_PORT.getName(), "9008");
+    env.put(CedarEnvironmentVariable.CEDAR_FOLDER_ADMIN_PORT.getName(), "9108");
+    env.put(CedarEnvironmentVariable.CEDAR_FOLDER_STOP_PORT.getName(), "9208");
 
     TestUtil.setEnv(env);
   }
